4e39285fa671f90f342e31442428f70fb9799555
Julio Mattos <juliocastellobranco@gmail.com>
2022-10-10 21:44:33 -0300

    install axios/get info from backend

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,32 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styles';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import FilterCard from '../../components/FilterCard'
 import TaskCard from '../../components/TaskCard'
-const tasks = [{ "done": false, "created": "2021-10-23T20:05:31.643Z", "_id": "101", "macaddress": "9191", "type": 2, "title": "Futebol Com a Galera", "description": "Jogar Futebol", "when": "10/05/2021", "__v": 0 },
-    { "done": false, "created": "2021-10-23T20:05:31.643Z", "_id": "102", "macaddress": "6666", "type": 7, "title": "Comprar Café", "description": "Café é Vida", "when": "11/05/2021", "__v": 0 }]
+import api from '../../services/api';
+
+// const tasks = [{ "done": false, "created": "2021-10-23T20:05:31.643Z", "_id": "101", "macaddress": "9191", "type": 2, "title": "Futebol Com a Galera", "description": "Jogar Futebol", "when": "10/05/2021", "__v": 0 },
+    // { "done": false, "created": "2021-10-23T20:05:31.643Z", "_id": "102", "macaddress": "6666", "type": 7, "title": "Comprar Café", "description": "Café é Vida", "when": "11/05/2021", "__v": 0 }]
 
 
 function Home() {
+
+    const [filterActived, setFilterActived] = useState('all');
+    const [tasksApi,setTasksApi] = useState([])
+
+    api.get('/task/filter/all/321654').then(response => {
+        setTasksApi(response.data)
+    })
+    
+
     return (
         <S.Container>
             <Header />
             
             <S.FilterArea>
-            <FilterCard title="Todos" actived={false}></FilterCard>
-            <FilterCard title="Hoje" actived={true}></FilterCard>
-            <FilterCard title="Semana" actived={false}></FilterCard>
-            <FilterCard title="Mês" actived={false}></FilterCard>
-            <FilterCard title="Ano" actived={false}></FilterCard>
+            <button onClick={() => setFilterActived("all")}>
+            <FilterCard title="Todos" actived={filterActived=='all'}></FilterCard>
+            </button>
+            <button onClick={() => setFilterActived("today")}>
+            <FilterCard title="Hoje" actived={filterActived=='today'}></FilterCard>
+            </button>
+            <button onClick={() => setFilterActived("week")}>
+            <FilterCard title="Semana" actived={filterActived=='week'}></FilterCard>
+            </button>
+            <button onClick={() => setFilterActived("month")}>
+            <FilterCard title="Mês" actived={filterActived=='month'}></FilterCard>
+            </button>
+            <button onClick={() => setFilterActived("year")}>
+            <FilterCard title="Ano" actived={filterActived=='year'}></FilterCard>
+            </button>
             </S.FilterArea>
 
             <S.Title>
                 <h3>TAREFAS</h3>
             </S.Title>
             <S.Content>
-            {tasks.map((t) => <TaskCard key={t._id} type={t.type} title={t.title} when={t.when} done={t.done} ></TaskCard>)}
-            {/* {tasks.map((t) => <TaskCard key={t.title} type={t.type} title={t.title} when={t.when} done={t.done} ></TaskCard>)} */}
+            {/* {tasks.map((t) => <TaskCard key={t._id} type={t.type} title={t.title} when={t.when} done={t.done} ></TaskCard>)} */}
+            {tasksApi.map((t) => <TaskCard key={t._id} type={t.type} title={t.title} when={t.when} done={t.done} ></TaskCard>)}
 
             </S.Content>
             <Footer />
