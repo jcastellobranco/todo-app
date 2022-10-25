a6a6c78eeb49e79d5cd28905450dec1df471a714
Julio Mattos <juliocastellobranco@gmail.com>
2022-10-24 21:20:44 -0300

    link on navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,15 +2,15 @@ import React from 'react'
 import * as S from './styles';
 import logo from '../../assets/logo.png'
 import bell from '../../assets/bell.png'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Header() {
   return ( <S.Container>
     <S.LeftSide>
-    <img src={logo} alt='Logo empresa'></img>
+    <Link to="/"><img src={logo} alt='Logo empresa'></img></Link>
     </S.LeftSide>
     <S.RightSide>
-    <button>INICIO</button>
+    <button><Link to="/">INICIO</Link></button>
     <span className='dividir'></span>
     <button><Link to="/task">NOVA TAREFA</Link></button>
     <button><img src={bell} alt='Sino Notificação'></img><span>2</span></button>
